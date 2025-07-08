eb5c813b072de6ac47eafe21ca978b2d310193ac
dimasdwi15 <dimasdwinugroho15@gmail.com>
2025-07-08 13:42:37 +0700

    update main.js

--- main.js
+++ main.js
@@ -1,97 +1,68 @@
-const endpoint = "http://192.168.1.12";
+const endpoint = "https://7eac7d55-5878-4c7d-8ff4-c3e739f267ba-00-31ob90z8or16t.janeway.replit.dev/";
 
 function getLamp1() {
-   fetch(endpoint + "/lamp1", {
-      method: "GET"
-   }).then(response => response.text()).then(result => {
-      if (result == "ON") {
-         lampSatu.style.backgroundColor = "blue";
-         lampSatuImage.src = "./assets/lampu_on.png";
-         lampSatu.innerText = "ON";
-      } else {
-         lampSatu.style.backgroundColor = "red";
-         lampSatuImage.src = "./assets/lampu_off.png";
-         lampSatu.innerText = "OFF";
-      }
-   });
+   fetch(endpoint + "lamp1")
+      .then(response => response.text())
+      .then(result => {
+         lampSatu.style.backgroundColor = result === "ON" ? "blue" : "red";
+         lampSatuImage.src = result === "ON" ? "./assets/lampu_on.png" : "./assets/lampu_off.png";
+         lampSatu.innerText = result;
+      });
 }
-
-function getLamp1() {
-   fetch(endpoint + "/lamp1", {
-      method: "GET"
-   }).then(response => response.text()).then(result => {
-      if (result == "ON") {
-         lampSatu.style.backgroundColor = "blue";
-         lampSatuImage.src = "./assets/lampu_on.png";
-         lampSatu.innerText = "ON";
-      } else {
-         lampSatu.style.backgroundColor = "red";
-         lampSatuImage.src = "./assets/lampu_off.png";
-         lampSatu.innerText = "OFF";
-      }
-   });
-}
-
 
 function getLamp2() {
-   fetch(endpoint + "/lamp2", {
-      method: "GET"
-   }).then(response => response.text()).then(result => {
-      if (result == "ON") {
-         lampDua.style.backgroundColor = "blue";
-         lampDuaImage.src = "./assets/lampu_on.png";
-         lampDua.innerText = "ON";
-      } else {
-         lampDua.style.backgroundColor = "red";
-         lampDuaImage.src = "./assets/lampu_off.png";
-         lampDua.innerText = "OFF";
-      }
-   });
+   fetch(endpoint + "lamp2")
+      .then(response => response.text())
+      .then(result => {
+         lampDua.style.backgroundColor = result === "ON" ? "blue" : "red";
+         lampDuaImage.src = result === "ON" ? "./assets/lampu_on.png" : "./assets/lampu_off.png";
+         lampDua.innerText = result;
+      });
 }
-
 
 function getLamp3() {
-   fetch(endpoint + "/lamp3", {
-      method: "GET"
-   }).then(response => response.text()).then(result => {
-      if (result == "ON") {
-         lampTiga.style.backgroundColor = "blue";
-         lampTigaImage.src = "./assets/lampu_on.png";
-         lampTiga.innerText = "ON";
-      } else {
-         lampTiga.style.backgroundColor = "red";
-         lampTigaImage.src = "./assets/lampu_off.png";
-         lampTiga.innerText = "OFF";
-      }
-   });
+   fetch(endpoint + "lamp3")
+      .then(response => response.text())
+      .then(result => {
+         lampTiga.style.backgroundColor = result === "ON" ? "blue" : "red";
+         lampTigaImage.src = result === "ON" ? "./assets/lampu_on.png" : "./assets/lampu_off.png";
+         lampTiga.innerText = result;
+      });
 }
-
-
 function setLamp1() {
-   fetch(endpoint + "/lamp1", {
-      method: "POST"
-   }).then(response => response.text()).then(() => location.reload());
-
+   const newState = lampSatu.innerText === "ON" ? "OFF" : "ON";
+   fetch(endpoint + "lamp1", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: newState })
+   }).then(() => getLamp1());
 }
 
 function setLamp2() {
-   fetch(endpoint + "/lamp2", {
-      method: "POST"
-   }).then(response => response.text()).then(() => location.reload());
+   const newState = lampDua.innerText === "ON" ? "OFF" : "ON";
+   fetch(endpoint + "lamp2", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: newState })
+   }).then(() => getLamp2());
 }
 
 function setLamp3() {
-   fetch(endpoint + "/lamp3", {
-      method: "POST"
-   }).then(response => response.text()).then(() => location.reload());
+   const newState = lampTiga.innerText === "ON" ? "OFF" : "ON";
+   fetch(endpoint + "lamp3", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ status: newState })
+   }).then(() => getLamp3());
 }
+
 
 function setLamp4() {
-   lampEmpat.style.backgroundColor = "red"
-   lampEmpatImage.src = "assets/lampu_on.png"
+   lampEmpat.style.backgroundColor = "red";
+   lampEmpatImage.src = "assets/lampu_on.png";
 }
 
-
+// Panggil saat halaman pertama dibuka
 getLamp1();
 getLamp2();
 getLamp3();
